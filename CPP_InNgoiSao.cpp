31e95d7028a2b6031584c55b5e758ac7688f208b
SPC <SPC@21-LAB402>
2024-06-26 19:59:54 +0700

    Add project files.

---
+++ CPP_InNgoiSao.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Nhap so: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+            
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        for (int h = n; h > i; h--)
+        {
+            cout << "* ";
+
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
